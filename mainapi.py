8e688d4a12db278f55bafb687b43d5be75baef52
alameluk17 <alamukanna14@gmail.com>
2024-01-28 05:03:33 +0530

    Use env instead of hardcoded API keys.

--- mainapi.py
+++ mainapi.py
@@ -1,6 +1,127 @@
+import json
 import requests
 import urllib.parse as urlparse
 from fastapi import FastAPI
-
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
+# from tomtomapi import get_data, get_test_data, encode
+import tomtomapi
 app = FastAPI()
+key = os.getenv("TOMTOMAPIKEY")
+
+@app.get("/")
+async def root(string):
+    
+    return {"message": json.loads(string)}
+
+
+@app.get("/routing")
+async def travelroute(start,end,departAt=None,travelMode="car",routeType="fastest",traffic="true",avoid="unpavedRoads",vehicleCommercial="true"):
+    # jsonip = await tomtomapi.gettestdata()
+    # jsonip1 = await tomtomapi.getdata(jsonip)
+    start = str(await tomtomapi.encode(start))
+    end = str(await tomtomapi.encode(end))
+    if departAt== None:
+        #current time and date
+        departAt = tomtomapi.convertdatetime()
+    else:
+        departAt = tomtomapi.convertdatetime(departAt)
+
+    baseUrl = "https://api.tomtom.com/routing/1/calculateRoute/"
+    # print(type(jsonip1["start"]))
+    requestParams = (
+    urlparse.quote(start) + ":" + urlparse.quote(end) 
+    + "/json?routeType=" + routeType
+    + "&traffic=" + traffic
+    + "&travelMode=" + travelMode
+    + "&avoid=" +  avoid
+    + "&vehicleCommercial=" + vehicleCommercial
+    + "&departAt=" + urlparse.quote(departAt))
+ 
+    requestUrl = baseUrl + requestParams + "&key=" + key
+    print(requestUrl)
+    response = requests.get(requestUrl)
+    return {"message": response.json()}
+
+
+@app.get("/EVrouting")
+async def EVroute():
+    # jsonip = await tomtomapi.gettestdata()
+    # jsonip1 = await tomtomapi.getdata(jsonip)
+    headers = {
+    'Content-Type': 'application/json',
+}
+
+    json_data = {
+        'chargingParameters': {
+            'batteryCurve': [
+                {
+                    'stateOfChargeInkWh': 50.0,
+                    'maxPowerInkW': 200,
+                },
+                {
+                    'stateOfChargeInkWh': 70.0,
+                    'maxPowerInkW': 100,
+                },
+                {
+                    'stateOfChargeInkWh': 80.0,
+                    'maxPowerInkW': 40,
+                },
+            ],
+            'chargingConnectors': [
+                {
+                    'currentType': 'AC3',
+                    'plugTypes': [
+                        'IEC_62196_Type_2_Outlet',
+                        'IEC_62196_Type_2_Connector_Cable_Attached',
+                        'Combo_to_IEC_62196_Type_2_Base',
+                    ],
+                    'efficiency': 0.9,
+                    'baseLoadInkW': 0.2,
+                    'maxPowerInkW': 11,
+                },
+                {
+                    'currentType': 'DC',
+                    'plugTypes': [
+                        'IEC_62196_Type_2_Outlet',
+                        'IEC_62196_Type_2_Connector_Cable_Attached',
+                        'Combo_to_IEC_62196_Type_2_Base',
+                    ],
+                    'voltageRange': {
+                        'minVoltageInV': 0,
+                        'maxVoltageInV': 500,
+                    },
+                    'efficiency': 0.9,
+                    'baseLoadInkW': 0.2,
+                    'maxPowerInkW': 150,
+                },
+                {
+                    'currentType': 'DC',
+                    'plugTypes': [
+                        'IEC_62196_Type_2_Outlet',
+                        'IEC_62196_Type_2_Connector_Cable_Attached',
+                        'Combo_to_IEC_62196_Type_2_Base',
+                    ],
+                    'voltageRange': {
+                        'minVoltageInV': 500,
+                        'maxVoltageInV': 2000,
+                    },
+                    'efficiency': 0.9,
+                    'baseLoadInkW': 0.2,
+                },
+            ],
+            'chargingTimeOffsetInSec': 60,
+        },
+    }
+    locations = "lat,long:lat,long"
+    locations = jsonip1["start"] + ":" + jsonip1["end"]
+    response = requests.post(
+        f'https://api.tomtom.com/routing/1/calculateLongDistanceEVRoute/{locations}/json?key=qN86js1EGFaSWvQ28TASgkUuphaxAnbF&vehicleEngineType=electric&constantSpeedConsumptionInkWhPerHundredkm=32,10.87:77,18.01&currentChargeInkWh=20&maxChargeInkWh=40&minChargeAtDestinationInkWh=4&minChargeAtChargingStopsInkWh=4',
+        headers=headers,
+        json=json_data,
+    )
+    return {"message": response.json()}
+
+# Translation 
+
--- tomtomapi.py
+++ tomtomapi.py
@@ -1,10 +1,15 @@
+import json
 import requests
 import urllib.parse as urlparse
-from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+load_dotenv()
 from datetime import datetime
-key = "qN86js1EGFaSWvQ28TASgkUuphaxAnbF"         # API Key
+# from tomtomapi import get_data, get_test_data, encode
+key = os.getenv("TOMTOMAPIKEY")
 
 vehicletypes = {0:"car",1:"bus",2:"motorcycle",3:"bicycle",4:"pedestrian",5:"truck",6:"EV"}
+
 def convertdatetime(dt=0):
     if dt == 0:
         #no parameter - current datetime
@@ -12,11 +17,21 @@ def convertdatetime(dt=0):
     output_date = dt.strftime("%Y-%m-%dT%H:%M:%S")
     return output_date
        
-       
-app = FastAPI()
-
 # Route parameters
                 
+async def encode(location="Adyar, Chennai"):
+    
+    baseUrl = "https://api.tomtom.com/search/2/geocode"
+    query= urlparse.quote(location)
+ 
+    requestUrl = baseUrl + "/" + query+".json" + "?limit=1&key=" + key
+    response = requests.get(requestUrl)
+    r = response.json()
+    r1=r["results"]
+    latitude = r1[0]["position"]["lat"]
+    longitude = r1[0]["position"]["lon"]
+    st = str(latitude) + ","+ str(longitude)
+    return st
 
 async def getdata(jsonip):
     jsonip["start"] = await encode(jsonip["start"])
@@ -44,122 +59,3 @@ async def gettestdata():
           "departAt" : "null",         
           "vehicleCommercial" : "true" } 
     return jsonip
-
-@app.get("/")
-async def root():
-    
-    return {"message": "hello world"}
-
-
-@app.get("/routing")
-async def travelroute():
-    jsonip = await gettestdata()
-    jsonip1 = await getdata(jsonip)
-    baseUrl = "https://api.tomtom.com/routing/1/calculateRoute/"
-    print(type(jsonip1["start"]))
-    requestParams = (
-    urlparse.quote(jsonip1["start"] ) + ":" + urlparse.quote(jsonip1["end"] ) 
-    + "/json?routeType=" + jsonip1["routeType"]
-    + "&traffic=" + jsonip1["traffic"]
-    + "&travelMode=" + jsonip1["travelMode"]
-    + "&avoid=" +  jsonip1["avoid"] 
-    + "&vehicleCommercial=" + jsonip1["vehicleCommercial"]
-    + "&departAt=" + urlparse.quote(jsonip1["departAt"]))
- 
-    requestUrl = baseUrl + requestParams + "&key=" + key
-    print(requestUrl)
-    response = requests.get(requestUrl)
-    return {"message": response.json()}
-
-@app.get("/encoding")
-async def encode(location="Adyar, Chennai"):
-    
-    baseUrl = "https://api.tomtom.com/search/2/geocode"
-    query= urlparse.quote(location)
- 
-    requestUrl = baseUrl + "/" + query+".json" + "?limit=1&key=" + key
-    response = requests.get(requestUrl)
-    r = response.json()
-    r1=r["results"]
-    latitude = r1[0]["position"]["lat"]
-    longitude = r1[0]["position"]["lon"]
-    st = str(latitude) + ","+ str(longitude)
-    return st
-
-@app.get("/EVrouting")
-async def EVroute():
-    jsonip = await gettestdata()
-    jsonip1 = await getdata(jsonip)
-    headers = {
-    'Content-Type': 'application/json',
-}
-
-    json_data = {
-        'chargingParameters': {
-            'batteryCurve': [
-                {
-                    'stateOfChargeInkWh': 50.0,
-                    'maxPowerInkW': 200,
-                },
-                {
-                    'stateOfChargeInkWh': 70.0,
-                    'maxPowerInkW': 100,
-                },
-                {
-                    'stateOfChargeInkWh': 80.0,
-                    'maxPowerInkW': 40,
-                },
-            ],
-            'chargingConnectors': [
-                {
-                    'currentType': 'AC3',
-                    'plugTypes': [
-                        'IEC_62196_Type_2_Outlet',
-                        'IEC_62196_Type_2_Connector_Cable_Attached',
-                        'Combo_to_IEC_62196_Type_2_Base',
-                    ],
-                    'efficiency': 0.9,
-                    'baseLoadInkW': 0.2,
-                    'maxPowerInkW': 11,
-                },
-                {
-                    'currentType': 'DC',
-                    'plugTypes': [
-                        'IEC_62196_Type_2_Outlet',
-                        'IEC_62196_Type_2_Connector_Cable_Attached',
-                        'Combo_to_IEC_62196_Type_2_Base',
-                    ],
-                    'voltageRange': {
-                        'minVoltageInV': 0,
-                        'maxVoltageInV': 500,
-                    },
-                    'efficiency': 0.9,
-                    'baseLoadInkW': 0.2,
-                    'maxPowerInkW': 150,
-                },
-                {
-                    'currentType': 'DC',
-                    'plugTypes': [
-                        'IEC_62196_Type_2_Outlet',
-                        'IEC_62196_Type_2_Connector_Cable_Attached',
-                        'Combo_to_IEC_62196_Type_2_Base',
-                    ],
-                    'voltageRange': {
-                        'minVoltageInV': 500,
-                        'maxVoltageInV': 2000,
-                    },
-                    'efficiency': 0.9,
-                    'baseLoadInkW': 0.2,
-                },
-            ],
-            'chargingTimeOffsetInSec': 60,
-        },
-    }
-    locations = "lat,long:lat,long"
-    locations = jsonip1["start"] + ":" + jsonip1["end"]
-    response = requests.post(
-        f'https://api.tomtom.com/routing/1/calculateLongDistanceEVRoute/{locations}/json?key=qN86js1EGFaSWvQ28TASgkUuphaxAnbF&vehicleEngineType=electric&constantSpeedConsumptionInkWhPerHundredkm=32,10.87:77,18.01&currentChargeInkWh=20&maxChargeInkWh=40&minChargeAtDestinationInkWh=4&minChargeAtChargingStopsInkWh=4',
-        headers=headers,
-        json=json_data,
-    )
-    return {"message": response.json()}
